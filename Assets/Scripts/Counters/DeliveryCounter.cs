9eed7e108fa77201aa81c0b5c8a28113c96a7f7b
Daniel Collins <dptc98@gmail.com>
2023-07-21 23:35:07 +0100

    Delivery manager to track recipes

--- Assets/Scripts/Counters/DeliveryCounter.cs
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class DeliveryCounter : BaseCounter
 {
-
   public override void Interact(Player player)
   {
     if (player.HasKitchenObject())
     {
       if (player.GetKitchenObject().TryGetComponent(out PlateKitchenObject plateKitchenObject))
       {
+        DeliveryManager.Instance.DeliverRecipe(plateKitchenObject);
         player.GetKitchenObject().DestroySelf();
       }
     }
--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static DeliveryManager Instance { get; set; }
+  [SerializeField]
+  private RecipeListSO recipeListSO;
+  private List<RecipeSO> waitingRecipeList;
+  private int waitingRecipeListMaxLen = 4;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+  private float spawnRecipeTimer;
+  private float spawnRecipeTimerMax = 4f;
+ private void Awake() {
+    Instance = this;
+    waitingRecipeList = new List<RecipeSO>();
+    spawnRecipeTimer = 0;
+  }
+
+  private void Update() {
+    spawnRecipeTimer += Time.deltaTime;
+    if(spawnRecipeTimer >= spawnRecipeTimerMax && waitingRecipeList.Count < waitingRecipeListMaxLen) {
+      RecipeSO waitingRecipeSO = recipeListSO.recipes[Random.Range(0, recipeListSO.recipes.Count)];
+      waitingRecipeList.Add(waitingRecipeSO);
+      spawnRecipeTimer = 0;
     }
+  }
+
+  public void DeliverRecipe(PlateKitchenObject plate) {
+    foreach (RecipeSO waitingRecipeSO in waitingRecipeList) {
+        if(waitingRecipeSO.kitchenObjectSOList.Count == plate.GetKitchenObjectSOs().Count) {
+        bool plateContentsMatchRecipe = true;
+        foreach (KitchenObjectSO recipeKitchenObjectSO in waitingRecipeSO.kitchenObjectSOList) {
+          bool ingredientFound = false;
+          foreach (KitchenObjectSO plateKitchenObjectSO in plate.GetKitchenObjectSOs()) {
+                    if (plateKitchenObjectSO == recipeKitchenObjectSO) {
+              ingredientFound = true;
+              break;
+            }
+                }
+            if(!ingredientFound) {
+          plateContentsMatchRecipe = false;
+        }
+            }
+        if(plateContentsMatchRecipe) {
+          waitingRecipeList.Remove(waitingRecipeSO);
+          return;
+        }
+        }
+    }
+  }
 }
